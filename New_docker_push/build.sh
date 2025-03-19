dfdbe25d915f7047c746f3e437715ac16e764ff9
sujith-26 <sujithg.22it@kongu.edu>
2025-03-19 15:49:44 +0530

    Delete New_docker_push directory

--- New_docker_push/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Variables
-IMAGE_NAME="sujith2606/demo"
-TAG="latest"
-
-# Build Docker image
-docker build -t $IMAGE_NAME:$TAG .
-echo "Docker image $IMAGE_NAME:$TAG built successfully."
